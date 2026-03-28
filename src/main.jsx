10261ea49bc5b5477f7447b05fd2a8707af02c58
Juan Alvarez <123114067+Sokiro02@users.noreply.github.com>
2026-03-28 00:05:49 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+try {
+  import("./App.jsx")
+    .then(({ default: App }) => {
+      createRoot(document.getElementById("root")).render(
+        <StrictMode>
+          <App />
+        </StrictMode>
+      );
+    })
+    .catch((e) => {
+      document.body.innerHTML = `<pre style="color:red;padding:20px">${e.message}\n${e.stack}</pre>`;
+    });
+} catch (e) {
+  document.body.innerHTML = `<pre style="color:red;padding:20px">${e.message}\n${e.stack}</pre>`;
+}
